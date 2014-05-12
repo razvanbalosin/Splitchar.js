707465c8d99dbe650bcee8eeefa3c18d5728bc6d
Razvan Balosin <balosinrazvan@yahoo.com>
2014-05-12 12:27:37 +0100

    Updated to include new functionalities

--- js/splitchar.js
+++ js/splitchar.js
@@ -4,12 +4,24 @@
  */
 
 $.fn.splitchar = function() {
+    $('head').append('<link rel="stylesheet" href="http://emisfera.github.io/Splitchar.js/css/splitchar.css" type="text/css" />');
 	$('.splitchar').each(function (index) {
 	    var characters = $(this).text().split("");
 	    $this = $(this);
 	    $this.empty();
 	    $.each(characters, function (i, el) {
-	    	$this.append("<span class='splitchar' data-content='" + el + "'>" + el + "</span>");
+            if ( $this.hasClass("horizontal") ) {
+	    	$this.append("<span class='horizontal' data-content='" + el + "'>" + el + "</span>");
+            }
+            else if ( $this.hasClass("horizontal-tri") ) {
+	    	$this.append("<span class='horizontal-tri' data-content='" + el + "'>" + el + "</span>");
+            }
+            else if ( $this.hasClass("vertical") ) {
+            $this.append("<span class='vertical' data-content='" + el + "'>" + el + "</span>");    
+            }
+            else if ( $this.hasClass("vertical-tri") ) {
+            $this.append("<span class='vertical-tri' data-content='" + el + "'>" + el + "</span>");    
+            }            
 	    });
 	});
 }
